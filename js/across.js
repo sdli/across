8e3cba11fc0998d00f1a0e3b835f954c2fa63338
lishudong <sdli@negadna.com>
2016-10-27 20:48:16 +0800

    second

--- js/across.js
+++ js/across.js
@@ -5,7 +5,7 @@
 	 */
 	function targetInfo(){
 		this.id = arguments[0].id;
-		this.color = arguments[0].color;
+		this.color = arguments[0].color?arguments[0].color:'#ccc';
 		this.gap = arguments[0].gap?arguments[0].gap:0;
 		this.num = arguments[0].num?arguments[0].num:1;
 		this.file = arguments[0].type;
@@ -21,22 +21,25 @@
 
 	targetInfo.prototype.createITags= function(){
 		if(acrossAnimationLibrary[this.file]){
-			console.log('OK');
 			var tags=[],
 				cssRules = acrossAnimationLibrary[this.file].keyframes,
 				node = document.getElementById(this.id),
 				styles = acrossAnimationLibrary[this.file].style, 
 				tar = document.createElement('style');
 
+			try{
+				tar.appendChild(document.createTextNode(styles));
+			  }catch(ex){
+				tar.styleSheet.cssText = styles;
+			}
 			document.getElementsByTagName('head')[0].appendChild(tar);
 			targetSheet = document.styleSheets[document.styleSheets.length-1];
 			targetSheet.insertRule(cssRules,targetSheet.rules.length);
 
 			for(var j =0;j<this.num;j++){
 				var tempTag = document.createElement(this.tag);
-				for(var key in styles){
-					tempTag['style'][key] = styles[key];
-				}
+				tempTag.className = acrossAnimationLibrary[this.file].classname;
+				tempTag.style.backgroundColor = this.color;
 				tags.push(tempTag);
 			}
 			for(var i=0;i<tags.length;i++){
@@ -87,10 +90,26 @@
 
 	window.acrossAnimation = targetInfo;
 	window.acrossAnimationLibrary ={
-		loading1:{
-			"style":{display:'none',width: '4px',height: '35px',borderRadius:'2px',margin:'0 2px',backgroundColor: '#333'},
-			"keyframes":"@keyframes loading{0%{transform: scaleY(1);}50%{transform: scaleY(0.5);}100%{transform: scaleY(1);}}",
-			"animation":"loading 1s ease-in {!gap} infinite"},
-		loading2:{}
+		loadingLines:{
+			style:".loadingLines{display:none;width:2px;height:26px;border-radius:2px;margin:0 2px;}",
+			keyframes:"@keyframes loading{0%{transform: scaleY(1);}50%{transform: scaleY(0.5);}100%{transform: scaleY(1);}}",
+			animation:"loading 1s ease-in {!gap} infinite",
+			classname: 'loadingLines'
+		},
+		ballPulse:{
+			style:".ballPulse{width: 6px;height: 6px;border-radius: 100%;margin: 2px;-webkit-animation-fill-mode: both;animation-fill-mode: both;display: none;}",
+			keyframes:"@keyframes scale {0%{-webkit-transform: scale(1);transform: scale(1);opacity: 1; }45% {-webkit-transform: scale(0.1);transform: scale(0.1);opacity: 0.7; }80% {-webkit-transform: scale(1);transform: scale(1);opacity: 1; } }",
+			animation:"scale 0.75s {!gap} infinite cubic-bezier(.2, .68, .18, 1.08)",
+			classname: 'ballPulse'
+		},
+		nineBall:{
+			style:".nineBall{width: 4px;height: 4px;border-radius: 100%;margin: 1px;-webkit-animation-fill-mode: both;animation-fill-mode: both;display: none;}",
+			keyframes:"@keyframes nineBall{33% {-webkit-transform: translateY(10px);transform: translateY(10px);}66%{-webkit-transform: translateY(-10px);transform: translateY(-10px);}100% {-webkit-transform: translateY(0);transform: translateY(0);}}",
+			animation:"nineBall 0.6s {!gap} infinite ease-in-out",
+			classname: 'nineBall'
+		}
 };
 })(window);
+
+
+
